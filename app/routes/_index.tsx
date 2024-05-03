a3524c15472f8f543d237efb35569251a0914083
Felipe Mendes <felipemendesofficial.@gmail.com>
2024-05-03 20:32:49 -0300

    fix: Ajustando navbar e adicionando imagem de fundo

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -10,16 +10,16 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <div>
-      <nav className="bg-gray-800 shadow-lg">
-        <div className="max-w-7xl mx-auto px-4 flex justify-between items-center h-16">
+      <nav className="bg-blue-900">
+        <div className="max-w-5xl mx-auto px-4 flex h-16 w-full">
 
           {/* Logotipo */}
-          <div className="flex-shrink-0 flex items-center">
-            <img className="h-8" src="imagens/logo.png" alt="Logo" />
+          <div className="flex items-center">
+            <img className="h-11" src="imagens/logo.png" alt="Logo" />
           </div>
 
           {/* Links de navegação (centralizados) */}
-          <div className="flex justify-center flex-1 md:justify-end">
+          <div className="flex justify-center items-center flex-1 md:justify-center">
             <div className="hidden md:flex space-x-4">
               <a href="#segmentos" className="text-white hover:text-gray-300">Segmentos</a>
               <a href="#funcionalidades" className="text-white hover:text-gray-300">Funcionalidades</a>
@@ -27,8 +27,11 @@ export default function Index() {
               <a href="#sobre" className="text-white hover:text-gray-300">Sobre Nós</a>
               <a href="#blog" className="text-white hover:text-gray-300">Blog</a>
               <a href="#clientes" className="text-white hover:text-gray-300">Clientes</a>
-              <a href="#contato" className="text-white hover:text-gray-300 hidden md:block">Entre em Contato</a>
             </div>
+          </div>
+
+          <div className="flex justify-center items-center">
+            <a href="#contato" className="text-gray-800 bg-white rounded-md p-1 hover:text-gray-500 hidden md:block">Entre em Contato</a>
           </div>
 
 
@@ -42,6 +45,22 @@ export default function Index() {
           </div>
         </div>
       </nav>
+      
+      <section className="relative">
+        <img className="w-full h-auto" src="imagens/fundo-inicial.png" alt="Imagem de fundo" />
+
+        <div className="absolute inset-0 mx-auto flex max-w-3xl flex-col items-center justify-center text-center text-white">
+
+          <h1 className="mb-4 text-3xl font-bold leading-tight md:text-4xl lg:text-5xl">
+            Sistema Especializado em <br /> Industria, Comércio, Saúde e Distribuição
+          </h1>
+
+          <p className="mb-4 text-base md:text-lg lg:text-xl">
+            Gestão para tornar o seu negócio mais lucrativo
+          </p>
+          <a href="#contato" className="rounded-md bg-white p-1 text-gray-800 hover:text-gray-500 md:block" > Entre em Contato </a>
+        </div>
+      </section>
     </div>
   );
 }
