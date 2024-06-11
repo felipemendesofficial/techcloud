5442747c9e3c5fee27764bebf888af9b3844eb26
Felipe Mendes <felipemendesofficial@gmail.com>
2024-06-11 00:12:00 -0300

    update

--- app/components/cover.tsx
+++ app/components/cover.tsx
@@ -1,20 +1,28 @@
-export function Cover(){
-    return(
-        <>
-        <section className="relative">
-            <img className="w-full h-auto" src="imagens/fundo-inicial.png" alt="Imagem de fundo" />
-            <div className="absolute inset-0 mx-auto flex max-w-3xl flex-col items-center justify-center text-center text-white">
-                <h1 className="mb-4 text-xl font-bold leading-tight sm:text-4xl lg:text-6xl">
-                    Sistema Especializado em <br /> Industria, Comércio, Saúde e Distribuição
-                </h1>
-                <p className="mb-4 text-xs sm:text-xl lg:text-2xl">
-                    Gestão para tornar o seu negócio mais lucrativo
-                </p>
-                <a href="#contato" className="text-xs sm:text-lg rounded-md bg-white p-1 text-gray-600 hover:text-gray-400" >
-                    Entre em Contato
-                </a>
-            </div>
-        </section>
-        </>
-    )
+export function Cover() {
+  return (
+    <>
+      <section className="relative">
+        <img
+          className="w-full h-auto"
+          src="imagens/fundo-inicial.png"
+          alt="Imagem de fundo"
+        />
+        <div className="absolute inset-0 mx-auto flex max-w-3xl flex-col items-center justify-center gap-4 text-center text-white">
+          <h1 className="text-2xl font-bold leading-tight sm:text-4xl lg:text-6xl">
+            Sistema Especializado em <br /> Industria, Comércio, Saúde e
+            Distribuição
+          </h1>
+          <p className="text-sm sm:text-xl lg:text-2xl bg-gray-600 bg-opacity-55 p-2 rounded-2xl">
+            Gestão para tornar o seu negócio mais lucrativo
+          </p>
+          <a
+            href="#contato"
+            className="mt-6 text-xs sm:text-lg rounded-md bg-white p-1 text-gray-600 hover:text-gray-400"
+          >
+            Entre em Contato
+          </a>
+        </div>
+      </section>
+    </>
+  );
 }
--- app/components/segments.tsx
+++ app/components/segments.tsx
@@ -1,47 +1,59 @@
-export function Segments(){
-    return(
-        <>
-        <section className="bg-gray-200 flex items-center justify-center py-10">
-          <div className="p-4">
-            <h1 className=" flex justify-center text-lg font-medium text-gray-600">
-              O Sistema ERP ideal para
-            </h1>
-            <div className="flex flex-col justify-center items-center p-4 gap-4">
-              <div className="w-40 rounded-2xl bg-white shadow-lg hover:shadow-2xl p-3">
-                <div className="mx-auto h-12 w-12 rounded-full bg-blue-800 p-2">
-                  <img className="h-full w-full object-cover" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxNiIgaGVpZ2h0PSIxNiIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9IiNmZmZmZmYiIHN0cm9rZS13aWR0aD0iMC43NSIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2UtbGluZWpvaW49InJvdW5kIiBjbGFzcz0ibHVjaWRlIGx1Y2lkZS1mYWN0b3J5Ij48cGF0aCBkPSJNMiAyMGEyIDIgMCAwIDAgMiAyaDE2YTIgMiAwIDAgMCAyLTJWOGwtNyA1VjhsLTcgNVY0YTIgMiAwIDAgMC0yLTJINGEyIDIgMCAwIDAtMiAyWiIvPjxwYXRoIGQ9Ik0xNyAxOGgxIi8+PHBhdGggZD0iTTEyIDE4aDEiLz48cGF0aCBkPSJNNyAxOGgxIi8+PC9zdmc+"
-                  alt="Imagem do Card 1" />
-                </div>
-                <div className="mt-2">
-                  <p className="text-center text-xl font-bold text-gray-600">
-                    Industria
-                  </p>
-                </div>
+export function Segments() {
+  return (
+    <>
+      <section className="bg-gray-200 flex items-center justify-center py-10 lg:py-40">
+        <div className="max-w-5xl p-4">
+          <h1 className=" flex justify-center text-xl font-medium text-gray-600 md:text-3xl lg:text-4xl">
+            O Sistema ERP ideal para
+          </h1>
+
+          <div className="flex flex-col justify-center items-center p-4 gap-4 md:flex-row">
+            <div className="w-60 max-w-xs rounded-2xl bg-white shadow-lg hover:shadow-2xl p-6 sm:p-10 md:my-8 lg:w-80">
+              <div className="mx-auto h-16 w-16 rounded-full bg-blue-800 p-2 lg:h-24 lg:w-24 lg:p-4">
+                <img
+                  className="h-auto w-full object-cover"
+                  src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxNiIgaGVpZ2h0PSIxNiIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9IiNmZmZmZmYiIHN0cm9rZS13aWR0aD0iMC43NSIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2UtbGluZWpvaW49InJvdW5kIiBjbGFzcz0ibHVjaWRlIGx1Y2lkZS1mYWN0b3J5Ij48cGF0aCBkPSJNMiAyMGEyIDIgMCAwIDAgMiAyaDE2YTIgMiAwIDAgMCAyLTJWOGwtNyA1VjhsLTcgNVY0YTIgMiAwIDAgMC0yLTJINGEyIDIgMCAwIDAtMiAyWiIvPjxwYXRoIGQ9Ik0xNyAxOGgxIi8+PHBhdGggZD0iTTEyIDE4aDEiLz48cGF0aCBkPSJNNyAxOGgxIi8+PC9zdmc+"
+                  alt="Imagem do Card 1"
+                />
               </div>
-              <div className="w-40 rounded-2xl bg-white shadow-lg hover:shadow-2xl p-3">
-                <div className="mx-auto h-12 w-12 rounded-full bg-blue-800 p-2">
-                  <img className="h-full w-full object-cover" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxNiIgaGVpZ2h0PSIxNiIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9IiNmZmZmZmYiIHN0cm9rZS13aWR0aD0iMC43NSIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2UtbGluZWpvaW49InJvdW5kIiBjbGFzcz0ibHVjaWRlIGx1Y2lkZS1oZWFydC1wdWxzZSI+PHBhdGggZD0iTTE5IDE0YzEuNDktMS40NiAzLTMuMjEgMy01LjVBNS41IDUuNSAwIDAgMCAxNi41IDNjLTEuNzYgMC0zIC41LTQuNSAyLTEuNS0xLjUtMi43NC0yLTQuNS0yQTUuNSA1LjUgMCAwIDAgMiA4LjVjMCAyLjMgMS41IDQuMDUgMyA1LjVsNyA3WiIvPjxwYXRoIGQ9Ik0zLjIyIDEySDkuNWwuNS0xIDIgNC41IDItNyAxLjUgMy41aDUuMjciLz48L3N2Zz4=" alt="Imagem do Card 2"/>
-                </div>
-                <div className="mt-2">
-                  <p className="text-center text-xl font-bold text-gray-600">
-                    Saúde
-                  </p>
-                </div>
+              <div className="mt-2 lg:mt-4">
+                <p className="text-center text-xl font-bold text-gray-600 lg:text-3xl">
+                  Industria
+                </p>
               </div>
-              <div className="w-40 rounded-2xl bg-white shadow-lg hover:shadow-2xl p-3">
-                <div className="mx-auto h-12 w-12 rounded-full bg-blue-800 p-2">
-                  <img className="h-full w-full object-cover" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxNiIgaGVpZ2h0PSIxNiIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9IiNmZmZmZmYiIHN0cm9rZS13aWR0aD0iMC43NSIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2UtbGluZWpvaW49InJvdW5kIiBjbGFzcz0ibHVjaWRlIGx1Y2lkZS1wYWNrYWdlIj48cGF0aCBkPSJtNy41IDQuMjcgOSA1LjE1Ii8+PHBhdGggZD0iTTIxIDhhMiAyIDAgMCAwLTEtMS43M2wtNy00YTIgMiAwIDAgMC0yIDBsLTcgNEEyIDIgMCAwIDAgMyA4djhhMiAyIDAgMCAwIDEgMS43M2w3IDRhMiAyIDAgMCAwIDIgMGw3LTRBMiAyIDAgMCAwIDIxIDE2WiIvPjxwYXRoIGQ9Im0zLjMgNyA4LjcgNSA4LjctNSIvPjxwYXRoIGQ9Ik0xMiAyMlYxMiIvPjwvc3ZnPg=="
-                    alt="Imagem do Card 3" />
-                </div>
-                <div className="mt-2">
-                  <p className="text-center text-xl font-bold text-gray-600">
-                    Distribuição
-                  </p>
-                </div>
+            </div>
+
+            <div className="w-60 max-w-xs rounded-2xl bg-white shadow-lg hover:shadow-2xl p-6 sm:p-10 md:my-8 lg:w-80">
+              <div className="mx-auto h-16 w-16 rounded-full bg-blue-800 p-2 lg:h-24 lg:w-24 lg:p-4">
+                <img
+                  className="h-full w-full object-cover"
+                  src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxNiIgaGVpZ2h0PSIxNiIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9IiNmZmZmZmYiIHN0cm9rZS13aWR0aD0iMC43NSIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2UtbGluZWpvaW49InJvdW5kIiBjbGFzcz0ibHVjaWRlIGx1Y2lkZS1oZWFydC1wdWxzZSI+PHBhdGggZD0iTTE5IDE0YzEuNDktMS40NiAzLTMuMjEgMy01LjVBNS41IDUuNSAwIDAgMCAxNi41IDNjLTEuNzYgMC0zIC41LTQuNSAyLTEuNS0xLjUtMi43NC0yLTQuNS0yQTUuNSA1LjUgMCAwIDAgMiA4LjVjMCAyLjMgMS41IDQuMDUgMyA1LjVsNyA3WiIvPjxwYXRoIGQ9Ik0zLjIyIDEySDkuNWwuNS0xIDIgNC41IDItNyAxLjUgMy41aDUuMjciLz48L3N2Zz4="
+                  alt="Imagem do Card 2"
+                />
+              </div>
+              <div className="mt-2 lg:mt-4">
+                <p className="text-center text-xl font-bold text-gray-600 lg:text-3xl">
+                  Saúde
+                </p>
+              </div>
+            </div>
+            <div className="w-60 max-w-xs rounded-2xl bg-white shadow-lg hover:shadow-2xl p-6 sm:p-10 md:my-8 lg:w-80">
+              <div className="mx-auto h-16 w-16 rounded-full bg-blue-800 p-2 lg:h-24 lg:w-24 lg:p-4">
+                <img
+                  className="h-full w-full object-cover"
+                  src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxNiIgaGVpZ2h0PSIxNiIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9IiNmZmZmZmYiIHN0cm9rZS13aWR0aD0iMC43NSIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2UtbGluZWpvaW49InJvdW5kIiBjbGFzcz0ibHVjaWRlIGx1Y2lkZS1wYWNrYWdlIj48cGF0aCBkPSJtNy41IDQuMjcgOSA1LjE1Ii8+PHBhdGggZD0iTTIxIDhhMiAyIDAgMCAwLTEtMS43M2wtNy00YTIgMiAwIDAgMC0yIDBsLTcgNEEyIDIgMCAwIDAgMyA4djhhMiAyIDAgMCAwIDEgMS43M2w3IDRhMiAyIDAgMCAwIDIgMGw3LTRBMiAyIDAgMCAwIDIxIDE2WiIvPjxwYXRoIGQ9Im0zLjMgNyA4LjcgNSA4LjctNSIvPjxwYXRoIGQ9Ik0xMiAyMlYxMiIvPjwvc3ZnPg=="
+                  alt="Imagem do Card 3"
+                />
+              </div>
+              <div className="mt-2 lg:mt-4">
+                <p className="text-center text-xl font-bold text-gray-600 lg:text-3xl">
+                  Distribuição
+                </p>
               </div>
             </div>
           </div>
-        </section>
-        </>
-    )
+        </div>
+      </section>
+    </>
+  );
 }
